c308c0f9aa734ee8e7f27f679efc735119c90109
jcovarrubias11 <jcovarrubias10@mail.fresnostate.edu>
2021-07-30 14:41:08 -0700

    update test files :art:

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -6,31 +6,15 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 // import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:integration_test/integration_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 // import 'package:crazy_tweets_2/main.dart' as app;
 
-// void main() => run(_testMain);
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-// void _testMain() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     app.main();
-
-//     // Trigger a frame.
-//     await tester.pumpAndSettle();
-
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
-
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
-
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+  test("1 + 1 = 2", () async {
+    expect(1 + 1 == 2, true);
+  });
+}
